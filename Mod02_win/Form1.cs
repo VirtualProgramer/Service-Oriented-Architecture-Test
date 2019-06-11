00bfbc0c02cb639d141fc552499f51ff37eaea4b
VirtualProgramer <setnight7@gmail.com>
2019-06-11 14:08:22 +0800

    Add project files.

---
+++ Mod02_win/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mod02_win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            localhost.WebService ws = new localhost.WebService();
+            MessageBox.Show(ws.Add(5 , 13).ToString());
+        }
+    }
+}
